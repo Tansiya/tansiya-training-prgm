97b9395c363647cbe38bcc30b5b34725a03a91b5
Tansiya <tansiyamalarkodi3396@gmail.com>
2018-10-09 17:01:09 +0530

    tansi.py

--- divi.py
+++ divi.py
@@ -1,3 +1,42 @@
-for i in range(10):
-	print("I like flower")
-print("Good bye!")
+class tour:
+	 ooty = 10, 15
+
+	def ooty(self):
+		print("The number of male,female:",cls.ooty)
+		return;
+
+kodaikanal = 15, 20
+
+	def kodaikanal(self): 
+print=("The number of male,female:",cls.kodaikanal)
+return;
+
+kochin = 20, 15
+
+	def kochin(self):
+print=("The number of male,female:",cls.kochin)
+return;
+
+tour.printooty = classmethod(tour.printooty)
+tour.printooty()
+tour.printkodaikanal = classmethod(tour.printkodaikanal)
+tour.printkodaikanal()
+tour.printkochin = classmethod(tour.printkochin)
+tour.printkochin()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
