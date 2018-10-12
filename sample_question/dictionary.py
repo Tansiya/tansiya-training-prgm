21bfc1f3ceb9c7b8798e2d62acffb487b44198f6
Tansiya <tansiyamalarkodi3396@gmail.com>
2018-10-12 15:56:55 +0530

    dictionary.py

--- sample_question/dictionary.py
+++ sample_question/dictionary.py
@@ -1,10 +1,17 @@
-Find the dictionary integral values 1 and n.print the dictionary values.
+"""Find the dictionary integral values 1 and n.print the dictionary values."""
+
 """assign the input n and dictionary"""
+
 n = int(input())
 d = dict()
+
 """creating the for loop function"""
-	for i in range(1,n+1):
+
+for i in range(1,n+1):
+
 	d[i]=i*i
+
 """print the values"""
+
 print (d)
 
