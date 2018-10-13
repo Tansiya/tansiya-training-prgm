236ef34196c26c084ea6feab1874bfe245415896
Tansiya <tansiyamalarkodi3396@gmail.com>
2018-10-13 15:18:09 +0530

    alphabetical.py

---
+++ sample_question/alphabetical.py
@@ -0,0 +1,13 @@
+"""find the input and print.The words in sorting them alphabetically"""
+
+#creat items
+
+items = [x for x in input().split(',')]
+
+#the words are sorting
+
+items.sort()
+
+#print the items
+
+print(','.join(items))
