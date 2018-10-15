bdd98d741ed881e439da5ee4ddcccdb3bf5b6758
Tansiya <tansiyamalarkodi3396@gmail.com>
2018-10-15 16:42:19 +0530

    use_password

---
+++ sample_question/use_password.py
@@ -0,0 +1,34 @@
+""" Find website users input username and password to register.check validity of password input by users.1. At least 1 letter between [a-z]
+2. At least 1 number between [0-9]
+1. At least 1 letter between [A-Z]
+3. At least 1 character from [$#@]
+4. Minimum length of transaction password: 6
+5. Maximum length of transaction password: 12"""
+
+#import for use all math function
+import re
+value = []
+items=[x for x in input().split(',')]
+#access for all values
+for p in items:
+    if len(p)<6 or len(p)>12:
+        continue
+    else:
+        pass
+    if not re.search("[a-z]",p):
+        continue
+    elif not re.search("[0-9]",p):
+        continue
+    elif not re.search("[A-Z]",p):
+        continue
+    elif not re.search("[$#@]",p):
+        continue
+    elif re.search("\s",p):
+        continue
+    else:
+        pass
+    value.append(p)
+#print validity of password and user
+print (",".join(value))
+
+
