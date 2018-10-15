067b0db83b1c2186736237af6c943ac437c0c764
Tansiya <tansiyamalarkodi3396@gmail.com>
2018-10-15 11:53:38 +0530

    alphanumerical.py

--- sample_question/alphanumerical.py
+++ sample_question/alphanumerical.py
@@ -1,8 +1,8 @@
-"""Find the words as input and print the words after removing all duplicate words and sorting them alphanumerically"""
+"""Find the words as input and print the words after removing all duplicate words and sort   them alphanumerical"""
 
-#assign a input
-n = input()
-words = [word for word in s.split(',')]
+#assign input
+s = input()
+words = [word for word in s.split(" ")]
 
-	#print the words
-print(''.join(sorted(list(set(words)))) 
+#print the sorted words
+print(','.join(sorted(list(set(words)))))
