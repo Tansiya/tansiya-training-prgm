c4dd4a4ae471b284f5dc5e000473f2d622c8c2d2
Tansiya <tansiyamalarkodi3396@gmail.com>
2018-10-16 10:06:42 +0530

    tuple_assendingorder

---
+++ sample_question/tuple_assendingorder.py
@@ -0,0 +1,18 @@
+"""Find the (name, age, height) tuplesby assending order where the name is string, age and height are numbers.the tuples are input by console"""
+from  operator import itemgetter, attrgetter
+
+#assign a list
+l = []
+
+#using while loop
+
+while True:
+	s = input()
+#using if loop
+	if not s:
+		break;
+#list append the tuple
+	l.append(tuple(s.split(","))) 
+
+#print the sorted tupleand asendingorder
+print (sorted(l, key = itemgetter (0, 1, 2)))
