b3944b028107095710e57375f40c7246956090f9
Tansiya <tansiyamalarkodi3396@gmail.com>
2018-10-17 10:54:52 +0530

    func_dictionary

---
+++ sample_question/func_dictionary.py
@@ -0,0 +1,14 @@
+"""function using dictionary key numbers are between 1 and 3 and the values are square of keys"""
+
+#create fuunction using dictionary
+def printDict():
+	d=dict()
+#use ** operator get power of number
+	d[1]=1
+	d[2]=2**2
+	d[3]=3**2
+	print(d)
+		
+#print dictionary key number square value
+printDict()
+
