a3fdfa54695435377182ed9c327879e1f25a9e79
Tansiya <tansiyamalarkodi3396@gmail.com>
2018-10-17 10:19:21 +0530

    two_string.py

---
+++ sample_question/two_string.py
@@ -0,0 +1,6 @@
+"""Define a function that can accept two strings as input and them print the concatenate"""
+#create function using string
+def printValue(s1,s2):
+	print (s1+s2)
+#input print the concatenate
+printValue("3","4") 
