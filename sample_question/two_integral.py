be1e31e699b9982722c84a260c07a5f27c4439eb
Tansiya <tansiyamalarkodi3396@gmail.com>
2018-10-17 10:08:41 +0530

    two_integral.py

---
+++ sample_question/two_integral.py
@@ -0,0 +1,8 @@
+"""define a function that can receive two integral numbers in string form and print the sum"""
+
+#creating function
+def printValue(s1,s2):
+#convert a string to integer
+	print (int(s1)+int(s2))
+
+printValue("3","4") 
