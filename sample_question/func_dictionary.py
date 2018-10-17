1a5614ff4c10ef6d877a2b895791d42a2836c65f
Tansiya <tansiyamalarkodi3396@gmail.com>
2018-10-17 11:39:04 +0530

    func_dictionary

--- sample_question/func_dictionary.py
+++ sample_question/func_dictionary.py
@@ -1,6 +1,35 @@
-"""function using dictionary key numbers are between 1 and 3 and the values are square of keys"""
+"""function using dictionary key numbers are between 1 and 3 and another key number range between(1,21) the values are square of keys"""
 
-#create fuunction using dictionary
+#create fuunction using dictionarySolution
+def printDict():
+	d=dict()
+	for i in range(1,21):
+#use the ** operator to get power of number
+		d[i]=i**2
+#range of for loop
+	for (k,v) in d.items():
+#to get key/value pairs	
+		print (v)
+		
+
+printDict()
+
+
+def printDict():
+	d=dict()
+#range of for loop
+	for i in range(1,21):
+#use the ** operator to get power of number
+		d[i]=i**2
+	for k in d.keys():
+#to get key/values pairs	
+		print (k)
+		
+
+printDict()
+
+
+
 def printDict():
 	d=dict()
 #use ** operator get power of number
