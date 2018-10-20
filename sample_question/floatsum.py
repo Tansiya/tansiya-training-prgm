6af2558d01b1cd539c045e8aa12fb89a34837368
Tansiya <tansiyamalarkodi3396@gmail.com>
2018-10-20 16:32:49 +0530

    floatsum

---
+++ sample_question/floatsum.py
@@ -0,0 +1,12 @@
+"""Program to compute 1/2+2/3+3/4+....+n/n+1 with a given n input by console(n>0)"""
+
+#Create input function
+
+n=int(input())
+sum = 0.0
+#access the values for loop
+for i in range(1,n+1):
+    sum += float(float(i)/(i+1))
+print (sum)
+
+
