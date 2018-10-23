c4a5ea53db403eee68ef1b88b8ba5b25fbd85630
Tansiya <tansiyamalarkodi3396@gmail.com>
2018-10-23 12:20:01 +0530

    assert_even.py

---
+++ sample_question/assert_even.py
@@ -0,0 +1,14 @@
+"""We can write a program to  assert statements to verify that every number in the list [2,4,6,8] is even."""
+
+#assign a list
+
+li = [2,4,6,8]
+
+#access for values
+
+for i in li:
+
+#using assert statements
+
+    assert i%2==0
+print(li)
