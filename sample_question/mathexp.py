6bc15eba5684d5d0536aff9980af52313b9c15cb
Tansiya <tansiyamalarkodi3396@gmail.com>
2018-10-27 10:31:02 +0530

    mathexp.py

---
+++ sample_question/mathexp.py
@@ -0,0 +1,9 @@
+"""We can write a programm to accepts the basic mathematic expression by console input print the evaluation result"""
+
+#Define the expression
+
+expression = input()
+
+#Use evaluation to evaluation on expression
+
+print (eval(expression))
